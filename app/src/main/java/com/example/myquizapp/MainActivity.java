bf8495ac7dbe9650a49c08c647be32f3e3bd5ff2
lohoney <swatikumari130520@gmail.com>
2019-03-04 16:00:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/myquizapp/MainActivity.java
@@ -0,0 +1,13 @@
+gpackage com.example.myquizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
